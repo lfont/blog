3a97018d018e04b2db503810d58e7b883d5ef918
Loïc Fontaine <ljph.fontaine@gmail.com>
2012-09-05 15:25:16 +0200

    The http-server module was replaced by connect. jQuery is used to get the name of the twitter user. A leading slash is necessary to get the items of the theme.

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,23 @@
-var HTTPServer = require('http-server').HTTPServer,
-    colors = require('colors'),
+var colors = require('colors'),
+    connect = require('connect'),
+    fs = require('fs'),
 
-    httpServer = new HTTPServer({
-        root: './public/'
-    });
+    app = connect()
+        .use(connect.logger())
+        .use(connect.favicon('public/images/favicon.png'))
+        .use(connect.static('public'), { maxAge: 60000 })
+        .use(function (req, res, next) {
+            // The static middleware has not replyed so it's a 404 error.
+            res.statusCode = 404;
 
-httpServer.listen(process.env.PORT || 80);
+            fs.readFile('public/404.html', function (e, html) {
+                res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                res.end(html);
+            });
+        })
+        .listen(process.env.PORT || 80);
 
 process.on('SIGINT', function () {
-  console.log('http-server stopped.'.red);
+  console.log('server.js stopped.'.red);
   return process.exit();
 });
